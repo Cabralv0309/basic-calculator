9d9a5ad63f4e8edc689620a462242fa873828517
Victor Cabral <victorcabral0309@gmail.com>
2026-06-09 19:04:44 -0300

    Initial commit: adicionando código e documentação

---
+++ README.md
@@ -0,0 +1,27 @@
+# Calculadora Básica
+
+Calculadora web simples feita usando vanilla HTML5, CSS3 e JS.
+
+Esta calculadora foi feita como um _begginer project_ para aprender os básicos de _DOM manipulation_,
+_CSS styling_ e _event handling_.
+
+## Features
+
+- **Aritimética básica**: Cálculos simples como adição, subtração, multiplicação e divisão
+- **Apagar e/ou Clear**: Botão para deletar o último número ou `AC` para limpar toda a tela
+- **Porcentagem**: Botão especializado para converter números em suas respectivas porcentagens
+- **Suporte para números decimais**: Permite e realiza cálculos com números _float_
+
+## Screenshot
+
+![Preview](calculadora.png)
+
+## Feito com
+
+- HTML5: Estrutura semantica para os botões e o display
+- CSS3: Estilização do layout e design
+- JavaScript: _Event handling_ dos botões e controle do display
+
+## Como utilizar localmente
+
+Já que este projeto foi feito utilizando HTML e JS, não é necessário instalar nenhuma dependência.
---
+++ script.js
@@ -0,0 +1,86 @@
+import { evaluate } from "https://cdn.jsdelivr.net/npm/mathjs@14.8.1/+esm";
+
+const display = document.getElementById('display');
+
+
+
+//show numbers on display
+const btnNumbers = document.querySelectorAll('.number');
+btnNumbers.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let number = btn.dataset.value;
+        
+        if (display.innerText === '0' || display.innerText === 'Err') {
+            display.innerText = number;
+        } else{
+            display.innerText += number;
+        }
+    });
+});
+
+//adds operators to the display string
+const btnOperators = document.querySelectorAll('.operation');
+btnOperators.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (
+            display.innerText.at(-1) == '+' ||
+            display.innerText.at(-1) == '-' ||
+            display.innerText.at(-1) == 'x' ||
+            display.innerText.at(-1) == '÷' ||
+            display.innerText.at(-1) == comma.dataset.value
+        ) {
+            display.innerText = display.innerText.slice(0,-1); 
+            display.innerText += btn.dataset.value;
+        } else {
+            display.innerText += btn.dataset.value;
+        }
+    })
+})
+
+
+//AC logic
+const clear = document.getElementById('ac');
+clear.addEventListener('click', () => {
+    display.innerText = '0';
+})
+
+//backspace logic
+const del = document.getElementById('backspace');
+del.addEventListener('click', () => {
+    display.innerText = display.innerText.slice(0,-1);
+    if (display.innerText.length == 0){
+        display.innerText = '0';
+    }
+})
+
+//comma logic
+const comma = document.getElementById('comma');
+comma.addEventListener('click', () => {
+    let memory = display.innerText.split(/([\+\-x÷])/);
+    let cm = comma.dataset.value;
+    let lastNumber = memory.at(-1);
+    if (!lastNumber.includes(cm) && lastNumber !== ''){
+        display.innerText = display.innerText + cm;
+    }
+})
+
+//percetn logic
+const percent = document.getElementById('percent');
+percent.addEventListener('click', () => {
+    let memory = display.innerText.split(/([\+\-x÷])/);
+    if (memory[memory.length-1]!==''){
+        memory[memory.length-1] = Number(memory[memory.length-1])/100;
+    }
+    display.innerText = memory.join('');
+})
+
+//returns the operation result
+const equals = document.getElementById('equals');
+equals.addEventListener('click', () => {
+    let equation = display.innerText.replaceAll('x','*').replaceAll(',','.').replaceAll('÷','/');
+    let result = evaluate(equation);
+    if (result === Infinity || result === -Infinity || result === NaN) {
+        result = 'Err';
+    } else {result = String(result).replaceAll('.', ',');}
+    display.innerText = result;
+})
